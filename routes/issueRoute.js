0ef3da969651531e6d83a1b01019b2a50f5471a6
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-27 15:12:15 +0300

    post new issue

--- routes/issueRoute.js
+++ routes/issueRoute.js
@@ -1,3 +1,5 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = (app, db) => {
     app
         .route('/projects/:project')
@@ -8,9 +10,39 @@ module.exports = (app, db) => {
             // log(project);
             db.findOne({name: project}, (err, data) => {
                 if ( err ) log(err);
-                log(data)
+                // log(data)
                 res.render('projectDetail', {project: data})
             })
             
         })
+
+        // POST ISSUE
+        .post((req, res) => {
+            // const data = req.body;
+            let project =  req.params.project;
+            project = project.replace(':', '');
+
+            // console.log(project);
+            const { title, text, creator, assigned_to, status }  = req.body;
+
+            const newIssue = {
+                    _id: new ObjectID(),
+                    issue_title: title,
+                    issue_text: text,
+                    created_by: creator,
+                    assigned_to: assigned_to,
+                    status_text: status,
+                    created_on: Date.now(),
+                    updated_on: Date.now()
+            }
+
+
+
+            db.findOneAndUpdate({name: project}, {$push: {issues: newIssue}}, (err, doc) => {
+                if ( err ) console.log(err);
+                // console.log(doc);
+                res.redirect(`/projects/:${project}`)
+            })
+
+        }) // END OF POST
 }
