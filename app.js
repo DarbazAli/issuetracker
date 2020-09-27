11d67f90168733546c615756241250bcefb03b7d
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-27 13:09:01 +0300

    create index view

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const app = express();
 console.clear();
 const log = console.log;
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app
     .route('/')
-    .get( (req, res) => res.send('Hello world'))
+    .get( (req, res) => res.render('index'))
 
 
 const PORT = process.env.PORT || 3000
